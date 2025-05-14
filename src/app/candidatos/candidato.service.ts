52cd6bd86cbdbfd9abdfdb0f6e40559aea180fae
Maria Jose Bermudez <mj.bermudez2@uniandes.edu.co>
2025-05-14 08:46:57 -0500

    Funciones para obtener candidatos

---
+++ src/app/candidatos/candidato.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Candidato } from './candidato';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CandidatoService {
+
+  private apiUrl: string = 'https://raw.githubusercontent.com/k-garces/ISIS2603_202510_parcial2/refs/heads/main/jsons/candidates.json';
+
+constructor(private http: HttpClient) { }
+
+ getCandidatos(): Observable<Candidato[]> {
+    return this.http.get<Candidato[]>(this.apiUrl);
+  }
+
+
+}
